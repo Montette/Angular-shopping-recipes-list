62818be5facbfdd5a2dec478b5a2926790ee16f5
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-10-16 20:22:34 +0200

    add displaying recipe details after click on item in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() showDetails = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a test description', 'https://flockler.com/thumbs/sites/192/untitled_s600x600_c2318x1351_l0x1104.jpg'),
     new Recipe('A test2 recipe', 'This is a test2 description', 'https://flockler.com/thumbs/sites/192/untitled_s600x600_c2318x1351_l0x1104.jpg')
@@ -16,4 +17,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  recipeDetailsHandler(details: Recipe) {
+    console.log(details);
+    this.showDetails.emit(details);
+  }
 }
