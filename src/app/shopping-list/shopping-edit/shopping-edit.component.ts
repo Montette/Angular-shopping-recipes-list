fe6823fb5e7cc32f4fbae6f9657b2b90d383b78b
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-10-16 21:04:05 +0200

    add function of adding shopping list item to the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') ingredientNameRef: ElementRef;
+  @ViewChild('amountInput') ingredientAmountRef: ElementRef;
+  @Output() addIngredient = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  addIngredientHandler() {
+    const newIngredient = {name: this.ingredientNameRef.nativeElement.value, amount: this.ingredientAmountRef.nativeElement.value}
+    this.addIngredient.emit(newIngredient);
+  }
 }
